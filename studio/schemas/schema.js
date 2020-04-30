202ad78f6a668a6681b879103e351de775244c4b
jennyg3011 <61746491+jennyg3011@users.noreply.github.com>
2020-04-30 16:58:48 +0100

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,6 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
+import social from './documents/social'
 
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
