29a3c3e1965dc254e4bba11c4e42f2552f5d81c3
jennyg3011 <61746491+jennyg3011@users.noreply.github.com>
2020-04-30 17:16:59 +0100

    Update schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,6 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
+import category from './documents/categorynew'
 
 
 // Object types
