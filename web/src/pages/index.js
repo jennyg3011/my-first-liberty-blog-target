55df4d0818155121ca214fa9632979e65a8c34aa
jennyg3011 <61746491+jennyg3011@users.noreply.github.com>
2020-04-30 16:44:54 +0100

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -99,7 +99,7 @@ const IndexPage = props => {
         <h1 hidden>Welcome to {site.title}</h1>
         {postNodes && (
           <BlogPostPreviewList
-            title='Latest from Amrut!'
+            title='Latest from jenny!'
             nodes={postNodes}
             browseMoreHref='/archive/'
           />
